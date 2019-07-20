15f484301964067ad2bb90a4f70ccb9bdc711706
Jeniffer Carvalho <jenicarvalho@live.com>
2019-07-20 17:52:12 -0300

    iniciado aplicação

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Header";
+import { Cards } from "./styles";
+import GlobalStyle from "./globalStyles";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Header />
+      <Cards>
+        <div>
+          <h2>Léi</h2>
+          <p>
+            <strong>Definição:</strong> Substantivo masculino para uma unidade
+            de medida.
+          </p>
+          <p>
+            <strong>Sinônimos:</strong> Pedaço, um pouco, etc.
+          </p>
+        </div>
+        <div>
+          <h2>Léi</h2>
+          <p>
+            <strong>Definição:</strong> Substantivo masculino para uma unidade
+            de medida.
+          </p>
+          <p>
+            <strong>Sinônimos:</strong> Pedaço, um pouco, etc.
+          </p>
+        </div>
+        <div>
+          <h2>Léi</h2>
+          <p>
+            <strong>Definição:</strong> Substantivo masculino para uma unidade
+            de medida.
+          </p>
+          <p>
+            <strong>Sinônimos:</strong> Pedaço, um pouco, etc.
+          </p>
+        </div>
+        <div>
+          <h2>Léi</h2>
+          <p>
+            <strong>Definição:</strong> Substantivo masculino para uma unidade
+            de medida.
+          </p>
+          <p>
+            <strong>Sinônimos:</strong> Pedaço, um pouco, etc.
+          </p>
+        </div>
+        <div>
+          <h2>Léi</h2>
+          <p>
+            <strong>Definição:</strong> Substantivo masculino para uma unidade
+            de medida.
+          </p>
+          <p>
+            <strong>Sinônimos:</strong> Pedaço, um pouco, etc.
+          </p>
+        </div>
+        <div>
+          <h2>Léi</h2>
+          <p>
+            <strong>Definição:</strong> Substantivo masculino para uma unidade
+            de medida.
+          </p>
+          <p>
+            <strong>Sinônimos:</strong> Pedaço, um pouco, etc.
+          </p>
+        </div>
+      </Cards>
+    </>
   );
 }
 
