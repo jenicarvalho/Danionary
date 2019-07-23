a958a51c583e8ba8d9699e49731ff9cb24be568b
Jeniffer Carvalho <jeniffer@tecnologia2u.com.br>
2019-07-23 16:44:54 -0300

    Updates

---
+++ precache-manifest.9bf5bdea7d4989bf728d1582092cca07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebb1161e285e30257a1ccd71fff4fae7",
+    "url": "/Danionary/index.html"
+  },
+  {
+    "revision": "b0147d523024e79a503e",
+    "url": "/Danionary/static/js/2.c650c6e7.chunk.js"
+  },
+  {
+    "revision": "1cbb610acb46e690edc4",
+    "url": "/Danionary/static/js/main.163f3eed.chunk.js"
+  },
+  {
+    "revision": "1a1df190c0ae3ab1f4e7",
+    "url": "/Danionary/static/js/runtime~main.af4e209b.js"
+  },
+  {
+    "revision": "afc543fc9d4e26b7c62b84d7ff0af6aa",
+    "url": "/Danionary/static/media/logo-danionary.afc543fc.png"
+  }
+]);
